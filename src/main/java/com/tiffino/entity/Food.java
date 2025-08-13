404ca47c8758a03ac464ea909662a67b16fefece
Datta <kaledatta146@gmail.com>
2025-08-13 15:07:17 +0530

    feat:add @Setter in food entity

--- src/main/java/com/tiffino/entity/Food.java
+++ src/main/java/com/tiffino/entity/Food.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 @Entity
 @Table(name = "food")
 public class Food {
