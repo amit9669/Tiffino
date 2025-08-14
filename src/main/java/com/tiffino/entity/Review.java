7274490f0ecc1ec62a5003ece05c73d079d0369c
Datta <kaledatta146@gmail.com>
2025-08-14 10:52:09 +0530

    feat:add validation dependency

--- src/main/java/com/tiffino/entity/Review.java
+++ src/main/java/com/tiffino/entity/Review.java
@@ -25,9 +25,9 @@ public class Review {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "meal_id")
-    private Meal meal;
+//    @ManyToOne(optional = false)
+//    @JoinColumn(name = "meal_id")
+//    private Meal meal;
 
     @Min(value = 1, message = "Rating must be at least 1")
     @Max(value = 5, message = "Rating must not exceed 5")
